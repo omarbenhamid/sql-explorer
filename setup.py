fea47105ee12312f0147f1199aac498d8d408092
Omar BENHAMID <omar.benhamid@smart-gts.com>
2024-11-29 16:44:21 +0100

    Adjusting deps for django 3.2 on python 3.7

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3 :: Only",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.7",
     install_requires=[
         requirements("base.txt"),
     ],
